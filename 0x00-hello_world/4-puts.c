623ed4899567dd4c57cc1e824484e869e83113bd
Benneth Ubakanma <bennethubakanma@gmail.com>
2022-09-12 16:25:23 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly
+ *
+ * Return: Always 0 (Pass)
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
